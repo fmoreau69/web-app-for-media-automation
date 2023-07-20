8cd5e93d6e50a277ec976488f59aa220a51a25a1
fmoreau <fabien.moreau@ifsttar.fr>
2023-07-20 11:11:09 +0200

    Added options, help and about page

--- mysite/medias/urls.py
+++ mysite/medias/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     re_path(r'^clear/$', views.clear_database, name='clear_database'),
+    re_path(r'^set_options/$', views.set_options, name='set_options'),
+
     re_path(r'^upload/$', views.UploadView.as_view(), name='upload'),
+    re_path(r'^launch/$', views.UploadView.launch_process, name='launch'),
+
     re_path(r'^options/$', views.OptionsView.as_view(), name='options'),
+    re_path(r'^reset_options/$', views.reset_options, name='reset_options'),
+    re_path(r'^launch_with_options/$', views.OptionsView.launch_process_with_options, name='launch_with_options'),
+
     re_path(r'^process/$', views.ProcessView.as_view(), name='process')
 ]
--- mysite/medias/views.py
+++ mysite/medias/views.py
@@ -1,12 +1,11 @@
 import time
-import pandas as pd
 
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views import View
 
-from .forms import MediaForm
-from .models import Media  # , Option
+from .forms import MediaForm, OptionForm
+from .models import Media, Option
 
 
 class UploadView(View):
@@ -24,14 +23,22 @@ class UploadView(View):
             data = {'is_valid': False}
         return JsonResponse(data)
 
+    def launch_process(self, request):
+        msg = ''
+        for media in Media.objects.all():
+            msg = ('process launched for media :' + media)
+        return redirect(request.POST.get('next')), msg
+
 
 class OptionsView(View):
     def get(self, request):
+        if len(Option.objects.all()) == 0:
+            set_options()
         medias_list = Media.objects.all()
-        return render(self.request, 'medias/options/index.html', {'medias': medias_list})
+        option_list = Option.objects.all()
+        return render(self.request, 'medias/options/index.html', {'medias': medias_list, 'options': option_list})
 
     def post(self, request):
-        time.sleep(1)  # You don't need this line. This is just to delay the process, so you can see the progress bar
         form = MediaForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             media = form.save()
@@ -40,12 +47,12 @@ class OptionsView(View):
             data = {'is_valid': False}
         return JsonResponse(data)
 
-    def get_context_data(self, **kwargs):
-        context = super(OptionsView, self).get_context_data(**kwargs)
-        context['options'] = ["blur faces", "blur plates", "blur people", "blur cars", "conf", "boxes", "show_labels",
-                              "show_conf", "show"]
-        context['value'] = [True, True, False, False, 0.0, False, False, False, True]
-        return context
+    def launch_process_with_options(self, request):
+        msg = ''
+        for media in Media.objects.all():
+            msg = ('process launched for media :' + media)
+            # execute_from_command_line()
+        return redirect(request.POST.get('next')), msg
 
 
 class ProcessView(View):
@@ -54,8 +61,6 @@ class ProcessView(View):
         return render(self.request, 'medias/process/index.html', {'medias': medias_list})
 
     def post(self, request):
-        time.sleep(
-            1)  # You don't need this line. This is just to delay the process so you can see the progress bar testing locally.
         form = MediaForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             media = form.save()
@@ -70,3 +75,29 @@ def clear_database(request):
         media.file.delete()
         media.delete()
     return redirect(request.POST.get('next'))
+
+
+def reset_options(request):
+    for option in Option.objects.all():
+        option.delete()
+    set_options()
+    return redirect(request.POST.get('next'))
+
+
+def set_options():
+    options_list = [
+        {'title': "Blur faces", 'default': 1, 'value': 1, 'type': 'BOOL'},
+        {'title': "Blur plates", 'default': 1, 'value': 1, 'type': 'BOOL'},
+        {'title': "Blur people", 'default': 0, 'value': 0, 'type': 'BOOL'},
+        {'title': "Blur cars", 'default': 0, 'value': 0, 'type': 'BOOL'},
+        {'title': "Blur size", 'default': 0, 'value': 0, 'type': 'FLOAT'},  # , 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}
+        {'title': "ROI enlargement", 'default': 0, 'value': 0, 'type': 'FLOAT'},  # 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}},
+        {'title': "Detection threshold", 'default': 0, 'value': 0, 'type': 'FLOAT'},  # 'attr_list': {{'minimum': '0'}, {'maximum': '1'}}},
+        {'title': "Show", 'default': 1, 'value': 1, 'type': 'BOOL'},
+        {'title': "Show boxes", 'default': 0, 'value': 0, 'type': 'BOOL'},
+        {'title': "Show labels", 'default': 0, 'value': 0, 'type': 'BOOL'},
+        {'title': "Show conf", 'default': 0, 'value': 0, 'type': 'BOOL'}
+        ]
+    for option in options_list:
+        form = OptionForm(option)
+        form.save()
