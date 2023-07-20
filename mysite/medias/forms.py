80987fe2afbb459fac6f0607a9cc2d4139953ad6
fmoreau <fabien.moreau@ifsttar.fr>
2023-07-20 11:11:09 +0200

    Added options, help and about page

--- mysite/medias/forms.py
+++ mysite/medias/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Media  # , Option
+from .models import Media, Option
 
 
 class MediaForm(forms.ModelForm):
     class Meta:
         model = Media
         fields = ('file', )
+
+
+class OptionForm(forms.ModelForm):
+    class Meta:
+        model = Option
+        fields = "__all__"
--- mysite/medias/models.py
+++ mysite/medias/models.py
@@ -5,5 +5,22 @@ from django.db import models
 
 class Media(models.Model):
     title = models.CharField(max_length=255, blank=True)
-    file = models.FileField(upload_to='media/')
+    file = models.FileField(upload_to='input_media/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    duration = models.FloatField(default="0")
+    progress = models.IntegerField(default="0")
+
+
+class Option(models.Model):
+    title = models.CharField(max_length=255)
+    last_modified = models.DateTimeField(auto_now_add=True)
+    default = models.CharField(max_length=255, default="0", help_text="")
+    value = models.CharField(max_length=255, default="0", help_text="")
+    type = models.CharField(max_length=5, default="NULL", choices=(('BOOL', 'Boolean'), ('FLOAT', 'Float')), help_text="")
+    # attr_list = models.CharField(max_length=255, default="0", help_text="")
+
+    def __init__(self, *args, **kwargs):
+        super(Option, self).__init__( *args, **kwargs)
+
+    def __str__(self):
+        return f'{self.title} {self.value}'
