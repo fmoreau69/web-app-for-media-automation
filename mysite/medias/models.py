2081d92510edfa391c0327448db6ac4246b8e855
fmoreau <fabien.moreau@ifsttar.fr>
2023-07-28 18:08:38 +0200

    Updated option view and created media_table.html to isolate media table for easy updating

--- mysite/medias/models.py
+++ mysite/medias/models.py
@@ -7,16 +7,34 @@ class Media(models.Model):
     title = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='input_media/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    duration = models.IntegerField(default="0")
-    progress = models.IntegerField(default="0")
+    properties = models.CharField(max_length=255, blank=True)
+    duration_inSec = models.CharField(max_length=255, blank=True)
+    duration_inMinSec = models.CharField(max_length=255, blank=True)
+    progress = models.IntegerField(default=0)
+    blur_faces = models.BooleanField(default=True, verbose_name='Blur faces', help_text="")
+    blur_plates = models.BooleanField(default=True, verbose_name='Blur plates', help_text="")
+    blur_people = models.BooleanField(default=True, verbose_name='Blur people', help_text="")
+    blur_cars = models.BooleanField(default=True, verbose_name='Blur cars', help_text="")
+    blur_ratio = models.FloatField(default=0.2, verbose_name='Blur ratio', help_text="")
+    blur_size = models.FloatField(default=0.5, verbose_name='Blur size', help_text="")
+    ROI_enlargement = models.FloatField(default=0.5, verbose_name='ROI enlargement', help_text="")
+    Detection_threshold = models.FloatField(default=0.25, verbose_name='Detection threshold', help_text="")
+    show = models.BooleanField(default=True, verbose_name='Show', help_text="")
+    show_boxes = models.BooleanField(default=True, verbose_name='Show boxes', help_text="")
+    show_labels = models.BooleanField(default=True, verbose_name='Show labels', help_text="")
+    show_conf = models.BooleanField(default=True, verbose_name='Show conf', help_text="")
 
 
 class Option(models.Model):
     title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     default = models.CharField(max_length=255, default="0", help_text="")
     value = models.CharField(max_length=255, default="0", help_text="")
-    type = models.CharField(max_length=5, default="NULL", choices=(('BOOL', 'Boolean'), ('FLOAT', 'Float')), help_text="")
+    type = models.CharField(max_length=5, default="NULL", help_text="",
+                            choices=(('BOOL', 'Boolean'), ('FLOAT', 'Float')))
+    label = models.CharField(max_length=255, default="0", help_text="",
+                             choices=(('WTB', 'What to blur?'), ('HTB', 'How to blur?'), ('WTS', 'What to show?')))
     # attr_list = models.CharField(max_length=255, default="0", help_text="")
 
     def __init__(self, *args, **kwargs):
--- mysite/medias/views.py
+++ mysite/medias/views.py
@@ -1,8 +1,8 @@
-import time
 import cv2
 
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.template import loader
 from django.views import View
 
 from .forms import MediaForm, OptionForm
@@ -18,17 +18,37 @@ class UploadView(View):
         return render(self.request, 'medias/upload/index.html', {'medias': medias_list, 'options': option_list})
 
     def post(self, request):
-        # time.sleep(1)  # You don't need this line. This is just to delay the process, so you can see the progress bar
-        form = MediaForm(self.request.POST, self.request.FILES)
-        if form.is_valid():
-            media = form.save()
-            media.duration = cv2.VideoCapture('./media/' + media.file.name).get(cv2.CAP_PROP_POS_MSEC)
-            print('./media/' + media.file.name + str(media.duration))
+        print(self.request.POST)
+        media_form = MediaForm(self.request.POST, self.request.FILES)
+        if media_form.is_valid():
+            media = media_form.save()
+            vid = cv2.VideoCapture('./media/' + media.file.name)
+            media.fps = vid.get(cv2.CAP_PROP_FPS)
+            media.width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+            media.height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            media.properties = str(int(media.width)) + 'x' + str(int(media.height)) + ' (' + str(int(media.fps)) + 'fps)'
+            media.duration_inSec = vid.get(cv2.CAP_PROP_FRAME_COUNT)/media.fps
+            media.duration_inMinSec = str(int(media.duration_inSec / 60)) + ':' + str(media.duration_inSec % 60)
             media.save()
-            data = {'is_valid': True, 'name': media.file.name, 'url': media.file.url, 'duration': media.duration}
+            media_data = {'is_valid': True, 'name': media.file.name, 'url': media.file.url,
+                          'properties': media.properties, 'duration': media.duration_inMinSec}
         else:
-            data = {'is_valid': False}
-        return JsonResponse(data)
+            media_data = {'is_valid': False}
+        # option_form = OptionForm(self.request.POST, self.request.FILES)
+        # option = option_form.save()
+        # option.save()
+        # option_data = {'is_valid': True, 'title': option.title, 'value': option.value}
+        return JsonResponse(media_data)  # , option_data
+
+    def update_options(self, request):
+        if self.request.POST:
+            val = {}
+            for opt in Option.objects.all():
+                if opt.name not in self.request.POST.keys():
+                    val[opt.name] = 0
+                else:
+                    val[opt.name] = self.request.POST[opt.name]
+            print(val)
 
     def launch_process(self, request):
         msg = ''
@@ -36,31 +56,6 @@ class UploadView(View):
             msg = ('process launched for media :' + media)
             # execute_from_command_line()
         return redirect(request.POST.get('next')), msg
-
-    def launch_process_with_options(self, request):
-        msg = ''
-        for media in Media.objects.all():
-            msg = ('process launched for media :' + media)
-            # execute_from_command_line()
-        return redirect(request.POST.get('next')), msg
-
-
-class OptionsView(View):
-    def get(self, request):
-        if len(Option.objects.all()) == 0:
-            set_options()
-        medias_list = Media.objects.all()
-        option_list = Option.objects.all()
-        return render(self.request, 'medias/options/index.html', {'medias': medias_list, 'options': option_list})
-
-    def post(self, request):
-        option_form = OptionForm(self.request.POST, self.request.FILES)
-        if option_form.is_valid():
-            option = option_form.save()
-            data = {'is_valid': True, 'name': option.title, 'value': option.value}
-        else:
-            data = {'is_valid': False}
-        return JsonResponse(data)
 
     def launch_process_with_options(self, request):
         msg = ''
@@ -85,6 +80,13 @@ class ProcessView(View):
         return JsonResponse(data)
 
 
+def refresh_table(request):
+    medias_list = Media.objects.all()
+    template = loader.get_template('medias/upload/media_table.html')
+    response = {'render': template.render({'medias': medias_list}, request), }
+    return JsonResponse(response)
+
+
 def clear_database(request):
     for media in Media.objects.all():
         media.file.delete()
@@ -101,17 +103,18 @@ def reset_options(request):
 
 def set_options():
     options_list = [
-        {'title': "Blur faces", 'default': 1, 'value': 1, 'type': 'BOOL'},
-        {'title': "Blur plates", 'default': 1, 'value': 1, 'type': 'BOOL'},
-        {'title': "Blur people", 'default': 0, 'value': 0, 'type': 'BOOL'},
-        {'title': "Blur cars", 'default': 0, 'value': 0, 'type': 'BOOL'},
-        {'title': "Blur size", 'default': 1, 'value': 1, 'type': 'FLOAT'},  # , 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}
-        {'title': "ROI enlargement", 'default': 1, 'value': 1, 'type': 'FLOAT'},  # 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}},
-        {'title': "Detection threshold", 'default': 0, 'value': 0, 'type': 'FLOAT'},  # 'attr_list': {{'minimum': '0'}, {'maximum': '1'}}},
-        {'title': "Show", 'default': 1, 'value': 1, 'type': 'BOOL'},
-        {'title': "Show boxes", 'default': 0, 'value': 0, 'type': 'BOOL'},
-        {'title': "Show labels", 'default': 0, 'value': 0, 'type': 'BOOL'},
-        {'title': "Show conf", 'default': 0, 'value': 0, 'type': 'BOOL'}
+        {'title': "Blur faces", 'name': "blur_faces", 'default': 1, 'value': 1, 'type': 'BOOL', 'label': 'WTB'},
+        {'title': "Blur plates", 'name': "blur_plates", 'default': 1, 'value': 1, 'type': 'BOOL', 'label': 'WTB'},
+        {'title': "Blur people", 'name': "blur_people", 'default': 0, 'value': 0, 'type': 'BOOL', 'label': 'WTB'},
+        {'title': "Blur cars", 'name': "blur_cars", 'default': 0, 'value': 0, 'type': 'BOOL', 'label': 'WTB'},
+        {'title': "Blur ratio", 'name': "blur_ratio", 'default': 0.2, 'value': 0.2, 'type': 'FLOAT', 'label': 'HTB'},  # , 'attr_list': {{'minimum': '0'}, {'maximum': '100'}}
+        {'title': "Blur size", 'name': "blur_size", 'default': 0.5, 'value': 0.5, 'type': 'FLOAT', 'label': 'HTB'},  # , 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}
+        {'title': "ROI enlargement", 'name': "ROI_enlargement", 'default': 0.5, 'value': 0.5, 'type': 'FLOAT', 'label': 'HTB'},  # 'attr_list': {{'minimum': '1'}, {'maximum': '10'}}},
+        {'title': "Detection threshold", 'name': "detection_threshold", 'default': 0.25, 'value': 0.25, 'type': 'FLOAT', 'label': 'HTB'},  # 'attr_list': {{'minimum': '0'}, {'maximum': '1'}}},
+        {'title': "Show", 'name': "show", 'default': 1, 'value': 1, 'type': 'BOOL', 'label': 'WTS'},
+        {'title': "Show boxes", 'name': "show_boxes", 'default': 0, 'value': 0, 'type': 'BOOL', 'label': 'WTS'},
+        {'title': "Show labels", 'name': "show_labels", 'default': 0, 'value': 0, 'type': 'BOOL', 'label': 'WTS'},
+        {'title': "Show conf", 'name': "show_conf", 'default': 0, 'value': 0, 'type': 'BOOL', 'label': 'WTS'}
         ]
     for option in options_list:
         form = OptionForm(option)
