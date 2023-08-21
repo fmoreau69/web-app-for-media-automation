40b8a2985e7a7e89207574dc5692a65a6455727f
fmoreau <fabien.moreau@ifsttar.fr>
2023-08-21 18:54:27 +0200

    Added CheckboxMultipleModal to ask users which objects to blur from the list of model classes

--- mysite/medias/models.py
+++ mysite/medias/models.py
@@ -1,28 +1,39 @@
 from __future__ import unicode_literals
-
 from django.db import models
+
+from YOLOv8.ultralytics import YOLO
+import os
+
+
+def get_classes_name(model_path):
+    model = YOLO(model_path)
+    classes = (('face', 'Faces'), ('plate', 'Plates'),)
+    classes_values = model.model.names.values()
+    for class_value in classes_values:
+        classes = classes + ((class_value, class_value[0].upper() + class_value[1:] +
+                              ('s' if not class_value.endswith('s') else '')),)
+    return classes
 
 
 class Media(models.Model):
+    model_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "./YOLOv8/models/yolov8n.pt")
     title = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='input_media/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     properties = models.CharField(max_length=255, blank=True)
     duration_inSec = models.CharField(max_length=255, blank=True)
     duration_inMinSec = models.CharField(max_length=255, blank=True)
-    progress = models.IntegerField(default=0)
-    blur_faces = models.BooleanField(default=True, verbose_name='Blur faces', help_text="")
-    blur_plates = models.BooleanField(default=True, verbose_name='Blur plates', help_text="")
-    blur_people = models.BooleanField(default=True, verbose_name='Blur people', help_text="")
-    blur_cars = models.BooleanField(default=True, verbose_name='Blur cars', help_text="")
+    blur_progress = models.IntegerField(default=0, verbose_name='Blur progress', help_text="Blur progress")
     blur_ratio = models.FloatField(default=0.20, verbose_name='Blur ratio', help_text="")
     blur_size = models.FloatField(default=0.50, verbose_name='Blur size', help_text="")
     ROI_enlargement = models.FloatField(default=0.50, verbose_name='ROI enlargement', help_text="")
     Detection_threshold = models.FloatField(default=0.25, verbose_name='Detection threshold', help_text="")
-    show = models.BooleanField(default=True, verbose_name='Show', help_text="")
-    show_boxes = models.BooleanField(default=True, verbose_name='Show boxes', help_text="")
-    show_labels = models.BooleanField(default=True, verbose_name='Show labels', help_text="")
-    show_conf = models.BooleanField(default=True, verbose_name='Show conf', help_text="")
+    show = models.BooleanField(default=True, verbose_name='Show', help_text="Show visualization")
+    show_boxes = models.BooleanField(default=True, verbose_name='Show boxes', help_text="Show boxes from detection")
+    show_labels = models.BooleanField(default=True, verbose_name='Show labels', help_text="Show labels from detection")
+    show_conf = models.BooleanField(default=True, verbose_name='Show conf', help_text="Show confidence from detection")
+    classes2blur = models.CharField(max_length=14, null=True, verbose_name='Objects to blur',
+                                    choices=(get_classes_name(model_path)), help_text="Choose objects you want to blur")
 
 
 class Option(models.Model):
@@ -34,7 +45,7 @@ class Option(models.Model):
     type = models.CharField(max_length=5, default="NULL", help_text="",
                             choices=(('BOOL', 'Boolean'), ('FLOAT', 'Float')))
     label = models.CharField(max_length=255, default="0", help_text="",
-                             choices=(('WTB', 'What to blur?'), ('HTB', 'How to blur?'), ('WTS', 'What to show?')))
+                             choices=(('WTB', 'What to blur ?'), ('HTB', 'How to blur ?'), ('WTS', 'What to show ?')))
     # attr_list = models.CharField(max_length=255, default="0", help_text="")
 
     def __init__(self, *args, **kwargs):
--- mysite/medias/urls.py
+++ mysite/medias/urls.py
@@ -3,15 +3,16 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
+    re_path(r'^upload_from_url/$', views.upload_from_url, name='upload_from_url'),
     re_path(r'^clear/$', views.clear_database, name='clear_database'),
+    re_path(r'^refresh_content/$', views.refresh_content, name='refresh_content'),
     re_path(r'^refresh_table/$', views.refresh_table, name='refresh_table'),
+    re_path(r'^refresh_options/$', views.refresh_options, name='refresh_options'),
     re_path(r'^set_options/$', views.set_options, name='set_options'),
     re_path(r'^reset_options/$', views.reset_options, name='reset_options'),
 
     re_path(r'^upload/$', views.UploadView.as_view(), name='upload'),
-    re_path(r'^launch/$', views.UploadView.launch_process, name='launch'),
-    # re_path(r'^update_options/$', views.UploadView.update_options, name='update_options'),
-    re_path(r'^launch_with_options/$', views.UploadView.launch_process_with_options, name='launch_with_options'),
 
-    re_path(r'^process/$', views.ProcessView.as_view(), name='process')
+    re_path(r'^process/$', views.ProcessView.as_view(), name='process'),
+    # re_path(r'^launch/$', views.ProcessView.as_view(), name='launch'),
 ]
