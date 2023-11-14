20596b39a84b82bcfa301d9b07a924df0936bbf9
fmoreau <fabien.moreau@ifsttar.fr>
2023-11-14 20:33:28 +0100

    Allowed anonymous user to use wama

---
+++ wama/medias/apps.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MediasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wama.medias'
