0eba3cf0cf942f66afe704540aaefb70e7d53bb4
fmoreau <fabien.moreau@ifsttar.fr>
2023-11-14 20:33:28 +0100

    Allowed anonymous user to use wama

--- mysite/accounts/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.weights.BigAutoField'
-    name = 'mysite.accounts'
---
+++ wama/accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.weights.BigAutoField'
+    name = 'wama.accounts'
