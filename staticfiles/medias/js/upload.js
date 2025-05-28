05ee4bb6e565330f50e311729fd24bfa4c62c1d0
fmoreau69 <fabien.moreau@univ-eiffel.fr>
2025-05-28 16:30:11 +0200

    Production updates

---
+++ staticfiles/medias/js/upload.js
@@ -0,0 +1,45 @@
+$(function () {
+
+  $(".js-upload-medias").click(function () {
+    $("#fileupload").click();
+  });
+
+  $("#fileupload").fileupload({
+    dataType: 'json',
+    sequentialUploads: true,
+
+    start: function (e) {
+      $("#modal-progress").modal("show");
+    },
+
+    stop: function (e) {
+      $("#modal-progress").modal("hide");
+    },
+
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      var strProgress = progress + "%";
+      $(".progress-bar").css({"width": strProgress});
+      $(".progress-bar").text(strProgress);
+    },
+
+    done: function (e, data) {
+      if (data.result.is_valid) {
+        $("#gallery tbody").prepend(
+          "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+        )
+      }
+    }
+
+  }).bind('fileuploaddone', function (e, data) {
+    $.ajax({
+      type: 'GET',
+      url: '/medias/refresh/',
+      data: {template_name: 'content'},
+      success: function (res) {
+        $("#main_container")[0].innerHTML = res['render'];
+      }
+    });
+  });
+
+});
