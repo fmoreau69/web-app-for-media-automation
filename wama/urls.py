81c3c77ed5b20fc9731f1c27fa1269876315cc29
fmoreau69 <fabien.moreau@univ-eiffel.fr>
2025-05-28 16:30:11 +0200

    Production updates

--- wama/urls.py
+++ wama/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import include
+from django.contrib import admin
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -14,7 +15,10 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name='about/index.html'), name='about'),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
     # path('celery-progress/', include('celery_progress.urls')),
+    path('ping/', lambda request: HttpResponse("Django OK via Apache")),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
